82b15a40af37069bf2a6239ba1a1ddb0614da3ac
Nicodragon25 <98363397+Nicodragon25@users.noreply.github.com>
2022-02-02 00:42:52 -0300

    Movement and shooting scripts

---
+++ Sogeking/Assets/Scripts/ArrowGenerator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    public GameObject player;
+
+    bool canShoot = true;
+    public float shootingCoolDown;
+    float timePass;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canShoot == true)
+        {
+            Shoot(player.transform.localRotation.eulerAngles);
+            canShoot = false;
+        }
+        if (!canShoot)
+        {
+            timePass += Time.deltaTime;
+        }
+        if (timePass >= shootingCoolDown)
+        {
+            canShoot = true;
+            timePass = 0;
+        }
+    }
+
+    private void Shoot(Vector3 Rotation)
+    {
+        Instantiate(arrowPrefab, transform.position, transform.rotation = Quaternion.Euler(Rotation));
+    }
+}

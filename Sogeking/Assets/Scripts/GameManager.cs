492305460cd02be14bd826d5f461b0a8ab029e27
Nicodragon25 <98363397+Nicodragon25@users.noreply.github.com>
2022-04-07 11:47:26 -0300

    terrain mess

--- Sogeking/Assets/Scripts/GameManager.cs
+++ Sogeking/Assets/Scripts/GameManager.cs
@@ -47,7 +47,6 @@ public class GameManager : MonoBehaviour
         Instance.powerBar.minValue = player.GetComponent<PlayerController>().minArrowSpeed - 10;
         Instance.powerBar.maxValue = player.GetComponent<PlayerController>().maxArrowSpeed;
 
-        doorHp = door.GetComponent<DoorController>().doorHP;
         Instance.doorHpBar.maxValue = doorHp;
         Instance.doorHpBar.value = doorHp;
     }
@@ -55,8 +54,11 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1;
         powerBar.gameObject.SetActive(false);
-        //powerBar.minValue = player.GetComponent<PlayerController>().minArrowSpeed - 10;
-        //powerBar.maxValue = player.GetComponent<PlayerController>().maxArrowSpeed;
+
+        if (door.GetComponent<DoorController>())
+        {
+        doorHp = door.GetComponent<DoorController>().doorHP;
+        }
     }
 
     // Update is called once per frame
